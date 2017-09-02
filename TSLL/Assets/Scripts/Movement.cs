4edcce2f9c97292cc496776b97a68b486f659db3
Andres Ruiz <rb.andres98@gmail.com>
2017-09-02 13:45:11 -0500

    Clase

---
+++ TSLL/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float speed = 0.09f;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.position = transform.position - transform.right * speed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += transform.right * speed;
+		}
+	}
+}
